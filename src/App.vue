5c578928abc6cfe83dcf5f4eb334ea353938bb97
Jelatz <lanetejlad@gmail.com>
2025-09-30 21:10:33 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,12 @@
 
 <template>
   <div class="min-h-dvh relative">
-    <img src="/public/meme.jpg" alt="" class="absolute bottom-0 right-0 w-96 h-96 object-cover opacity-60" />
+    <div class="absolute inset-0 m-auto w-fit h-fit animate-pulse text-center">
+      <h1 class="text-6xl font-bold text-red-500">Still working on it!</h1>
+      <!-- <p class="text-center mt-3">Stay Tuned!</p> -->
+    </div>
 
+    <img src="/public/meme.jpg" alt="" class="absolute bottom-0 right-0 w-96 h-96 object-cover opacity-60" />
   </div>
 </template>
 
