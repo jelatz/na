bd852bc04c983be74c75c8e1159fcbafb02e92d3
Jelatz <lanetejlad@gmail.com>
2025-09-30 20:46:20 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="min-h-dvh relative">
-    <img src="/public/meme.jpg" alt="" class="absolute top-96 right-0 w-96 h-96 object-cover opacity-60" />
+    <img src="/public/meme.jpg" alt="" class="absolute bottom-0 right-0 w-96 h-96 object-cover opacity-60" />
 
   </div>
 </template>
