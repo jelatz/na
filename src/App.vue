a0c419999b2dcccdfc7079a27f393a352bbcca2a
Jelatz <lanetejlad@gmail.com>
2025-10-01 22:33:17 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
 <template>
   <div>
     <div class="min-h-dvh relative">
-      <div class="absolute inset-0 m-auto w-fit h-fit animate-pulse text-center p-10">
+      <!-- <div class="absolute inset-0 m-auto w-fit h-fit animate-pulse text-center p-10">
         <h1 class="text-6xl font-bold text-red-500">Work In-progress!</h1>
-        <!-- <p class="text-center mt-3">Stay Tuned!</p> -->
-      </div>
+      <p class="text-center mt-3">Stay Tuned!</p>
+    </div> -->
       <div class="flex flex-col items-center absolute bottom-0 left-1/2 -translate-x-1/2">
         <img src="/public/fast-forward.gif" alt=""
           class="w-20 h-20 object-cover opacity-60 rotate-90 absolute top-[3rem]" />
