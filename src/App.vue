5010ec6b66088f694da5f372e03f18d06bf419ff
Jelatz <lanetejlad@gmail.com>
2025-10-01 21:52:49 +0800

    added xmas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup></script>
 
 <template>
-  <div class="min-h-dvh relative">
-    <div class="absolute inset-0 m-auto w-fit h-fit animate-pulse text-center p-10">
-      <h1 class="text-6xl font-bold text-red-500">Work In-progress!</h1>
-      <!-- <p class="text-center mt-3">Stay Tuned!</p> -->
-    </div>
+  <div>
+    <div class="min-h-dvh relative">
+      <div class="absolute inset-0 m-auto w-fit h-fit animate-pulse text-center p-10">
+        <h1 class="text-6xl font-bold text-red-500">Work In-progress!</h1>
+        <!-- <p class="text-center mt-3">Stay Tuned!</p> -->
+      </div>
 
-    <img src="/public/meme.jpg" alt="" class="absolute bottom-0 right-0 w-96 h-96 object-cover opacity-60" />
+      <img src="/public/jose-marie.jpeg" alt=""
+        class="absolute bottom-0 left-1/2 -translate-x-1/2 w-96 h-96 object-cover opacity-60" />
+    </div>
+    <div>
+      <h1>hey</h1>
+    </div>
   </div>
 </template>
 
