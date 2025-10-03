d09ab6586f98836b8536b171f7fae690a3592df9
Jelatz <lanetejlad@gmail.com>
2025-10-03 19:51:47 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <script setup>
-// import { FastForwardIcon } from '@heroicons/vue/24/solid'
-
 </script>
 
 <template>
-  <div>
-    <div class="min-h-dvh relative">
-      <!-- <div class="absolute inset-0 m-auto w-fit h-fit animate-pulse text-center p-10">
-        <h1 class="text-6xl font-bold text-red-500">Work In-progress!</h1>
-      <p class="text-center mt-3">Stay Tuned!</p>
-    </div> -->
-      <div class="flex flex-col items-center absolute bottom-0 left-1/2 -translate-x-1/2">
-        <img src="/public/fast-forward.gif" alt=""
-          class="w-20 h-20 object-cover opacity-60 rotate-90 absolute top-[3rem]" />
-        <img src="/public/jose-marie.jpeg" alt="" class="w-96 h-96 object-cover opacity-60" />
-      </div>
+  <div class="min-h-dvh bg-black bg-[url('/public/bg.png')] bg-top bg-cover bg-no-repeat relative">
+    <!-- Hero Section -->
+    <div class="flex flex-col items-center pt-20 space-y-6 relative z-10">
+      <img src="/public/logo.png" alt="logo" class="w-[20rem] md:w-[30rem]" />
+      <img src="/public/topic.png" alt="topic" class="w-[30rem] md:w-[50rem]" />
     </div>
-    <div class="min-h-dvh bg-black grid place-items-center">
-      <h1 class="text-6xl font-bold text-white">Coming Soon!</h1>
+
+    <!-- Church Decoration -->
+    <img src="/public/church.png" alt="church"
+      class="w-[20rem] md:w-[40rem] opacity-20 absolute bottom-2 right-4 md:right-60" />
+
+    <!-- Content Section -->
+    <div class="flex flex-col justify-start items-center  space-y-10 relative z-10 px-6">
+      <div>
+        <!-- Block 1 -->
+        <div class="text-white max-w-2xl my-5">
+          <p class="font-extrabold text-2xl mb-3">From Pain to Purpose</p>
+          <ul class="list-disc list-inside space-y-2 text-lg">
+            <li>Butch A. - Redemtorist HG</li>
+            <li>Harold G. - Bliss HG</li>
+            <li>Jepoy E. - Bohol HG</li>
+            <li>Nyang S. - Kufa Habagatan HG</li>
+          </ul>
+        </div>
+
+        <!-- Block 2 -->
+        <div class="text-white max-w-2xl my-5">
+          <p class="font-extrabold text-2xl mb-3">The key to freedom is in the steps.</p>
+          <ul class="list-disc list-inside space-y-2 text-lg">
+            <li>Khim K. - </li>
+            <li>Jeffrey G. - Sleeping Dragons HG (Pardo)</li>
+            <li>Frankie T. - Isla HG</li>
+          </ul>
+        </div>
+
+        <!-- Block 3 -->
+        <div class="text-white max-w-2xl my-5">
+          <p class="font-extrabold text-2xl mb-3">Nothing changes if nothing changes</p>
+          <ul class="list-disc list-inside space-y-2 text-lg">
+            <li>Renniel B. - Isla HG</li>
+            <li>Stephen T. - Sleeping Dragon HG (Pardo)</li>
+            <li>Jerome C. - Bacayan HG</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
